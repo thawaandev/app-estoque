117f8bb600b308836be1a977803d8e7e0c3f5de6
Thawan Lucas <thawanlucascaze@hotmail.com>
2025-06-30 14:36:05 -0300

    created method for exceptions

---
+++ src/main/java/com/cadastro_estoque/app_estoque/exceptions/RecursoNaoEncontradoException.java
@@ -0,0 +1,9 @@
+package com.cadastro_estoque.app_estoque.exceptions;
+
+public class RecursoNaoEncontradoException extends RuntimeException{
+
+    public RecursoNaoEncontradoException(String message) {
+        super(message);
+    }
+
+}
