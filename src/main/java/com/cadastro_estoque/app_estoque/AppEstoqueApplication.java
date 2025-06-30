1f6e3d0966eb6410cf7f1e798860d6cb5892bfbc
Thawan Lucas <thawanlucascaze@hotmail.com>
2025-06-30 13:56:10 -0300

    commit inicial

---
+++ src/main/java/com/cadastro_estoque/app_estoque/AppEstoqueApplication.java
@@ -0,0 +1,13 @@
+package com.cadastro_estoque.app_estoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppEstoqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppEstoqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cadastro_estoque/app_estoque/AppEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cadastro_estoque.app_estoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppEstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
